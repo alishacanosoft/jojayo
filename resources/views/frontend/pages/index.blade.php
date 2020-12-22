d4c6deef1ab1a1430206bc0c2529c81904a21028
tsurajmaharjan <suraj@canosoft.com.np>
2020-12-22 12:52:43 +0545

    flash page design updated

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -151,10 +151,7 @@
                                       <option value="2">5</option>
                                     </select>
                       </div>
-                      <div class="ps-product__progress-bar ps-progress" data-value="37">
-                        <div class="ps-progress__value"><span></span></div>
-                        <p>Sold:16</p>
-                      </div>
+                      
                     </div>
                   </div>
                 </div>
@@ -420,32 +417,7 @@
             </div>
         </div> -->
 
-        <!-- <div class="ps-download-app">
-              <div class="ps-container">
-                  <div class="ps-block--download-app">
-                      <div class="container">
-                          <div class="row">
-                              <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 ">
-                                  <div class="ps-block__thumbnail"><img src="/frontend/images/app.png" alt=""></div>
-                              </div>
-                              <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 ">
-                                  <div class="ps-block__content">
-                                      <h3>Download Martfury App Now!</h3>
-                                      <p>Shopping fastly and easily more with our app. Get a link to download the app on your phone</p>
-                                      <form class="ps-form--download-app" action="http://nouthemes.net/html/martfury/do_action" method="post">
-                                          <div class="form-group--nest">
-                                              <input class="form-control" type="Email" placeholder="Email Address">
-                                              <button class="ps-btn">Subscribe</button>
-                                          </div>
-                                      </form>
-                                      <p class="download-link"><a href="#"><img src="/frontend/images/google-play.png" alt=""></a><a href="#"><img src="/frontend/images/app-store.png" alt=""></a></p>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-        </div> --> 
+  
 
         <!-- Hot arrivlas -->
         <div class="ps-product-list ps-new-arrivals">
