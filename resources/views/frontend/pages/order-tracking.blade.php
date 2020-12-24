1ed59f4ddf535e9b7533a85c0ea7d6e2528c924c
tsurajmaharjan <suraj@canosoft.com.np>
2020-12-24 13:53:55 +0545

    added design for oder not found

--- resources/views/frontend/pages/order-tracking.blade.php
+++ resources/views/frontend/pages/order-tracking.blade.php
@@ -36,6 +36,13 @@
 
       <div class="container order-details-container">
 
+          <div class="container order-not-found">
+              <div class="ps-section__content"><img src="{{asset('/frontend/images/order-not-found.png')}}" alt="jojayo-404-order-not-found">
+                <h3>Opps! Order Not Found</h3>
+              
+          </div>
+          </div>
+
           <div class="ps-section__header order-tracking">
             <h3>Your Order Details</h3>
           </div>
